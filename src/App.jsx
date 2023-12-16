50186e419475e7d5e954e1e21064e0fbab2d1ed5
thanhsondev <sondvtgcd201619@fpt.edu.vn>
2023-12-17 02:15:25 +0700

    update 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,70 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
-import EmployeeContextProvider from './contexts/employeeContext';
-import TechnicalContextProvider from './contexts/technicalContext';
-import ComponentsContextProvider from './contexts/componentsContext';
-import LayoutContextProvider from './contexts/LayoutContext';
+import AuthContextProvider from './contexts/authContext';
+import EmployeeContextProvider from "./contexts/employeeContext";
+import TechnicalContextProvider from "./contexts/technicalContext";
+import ComponentsContextProvider from "./contexts/componentsContext";
+import LayoutContextProvider from "./contexts/LayoutContext";
+import RecordContextProvider from './contexts/recordlogContext';
+import ProjectContextProvider from './contexts/projectContext';
+import RoleContextProvider from './contexts/roleContext';
 
-import { Layout } from './components/layout/Layout';
-import { Dashboard } from './pages/dashboard/Dashboard';
-import Employees from './pages/employee/employees';
-import EmployeeDetails from './pages/employee/employeeDetails';
-import PageNotFound from "./pages/404notfound/PageNotFound";
+import { Layout } from "./components/layout/Layout";
+import { Dashboard } from "./pages/dashboard/Dashboard";
+import Employees from "./pages/employee/employees";
+import EmployeeDetails from "./pages/employee/employeeDetails";
+import LogPage from "./pages/log/logPage";
+import LoginPage from "./pages/login/loginPage";
+import Projects from "./pages/project/projects";
+import ProjectDetails from "./pages/project/projectDetails";
+import AddProject from "./pages/project/addProject";
+import PageNotFound from "./pages/404notfound/PagenotFound";
+
+import { useContext } from 'react';
+import { AuthContext } from './contexts/authContext';
+
+const ProtectedRoute = ({ element }) => {
+   const { authState: { isAuthenticated } } = useContext(AuthContext);
+
+   return isAuthenticated ? element : <Navigate to="/login" />;
+};
+
 function App() {
    return (
-       <LayoutContextProvider>
-          <ComponentsContextProvider>
-             <TechnicalContextProvider>
+       <AuthContextProvider>
+          <LayoutContextProvider>
+             <ComponentsContextProvider>
                 <EmployeeContextProvider>
-                   <Routes>
-                      <Route path="/" element={<Layout />}>
-                         <Route path="dashboard" element={<Dashboard />} />
-                         <Route path="employee">
-                            <Route index element={<Employees />} />
-                            <Route path=":employeeId" element={<EmployeeDetails />} />
-                         </Route>
-                      </Route>
-                   </Routes>
+                   <TechnicalContextProvider>
+                      <RecordContextProvider>
+                         <ProjectContextProvider>
+                            <RoleContextProvider>
+                               <Routes>
+                                  <Route path="login" element={<LoginPage />} />
+                                  <Route path="/*" element={<PageNotFound />} />
+                                  <Route path="/" element={<ProtectedRoute element={<Layout />} />} >
+                                     <Route path="/" element={<Dashboard />} />
+                                     <Route path="/employee" element={<Employees />} />
+                                     <Route path="/employee/:employeeId" element={<EmployeeDetails />} />
+                                     <Route path="/project" element={<Projects />} />
+                                     <Route path="/addproject" element={<AddProject />} />
+                                     <Route path="/project/:projectId" element={<ProjectDetails />} />
+                                     <Route path="/log" element={<LogPage />} />
+
+                                  </Route>
+                               </Routes>
+                            </RoleContextProvider>
+                         </ProjectContextProvider>
+                      </RecordContextProvider>
+                   </TechnicalContextProvider>
                 </EmployeeContextProvider>
                 <Routes>
                    <Route path="/doesnt-exist" element={<PageNotFound/>} />
                 </Routes>
-             </TechnicalContextProvider>
-          </ComponentsContextProvider>
-       </LayoutContextProvider>
+             </ComponentsContextProvider>
+          </LayoutContextProvider>
+       </AuthContextProvider>
    );
 }
 
